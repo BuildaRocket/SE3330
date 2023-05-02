c643c43618b96fe170787dd831e49145ea297ce5
Du Xinmin <114809783+Doris-xm@users.noreply.github.com>
2023-05-02 11:29:18 +0800

    Add files via upload
    finish MomentView UI，create Menu in PersonView

---
+++ JiaoJi_app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import './css/backgroud.css'
+import PersonView from "./View/PersonView";
+
+function App() {
+  return (
+    <PersonView/>
+  );
+}
+
+export default App;
---
+++ JiaoJi_app/src/View/InfoView.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const InfoView: React.FC = () => {
+
+    return (
+        <div>
+            <h1>INFO</h1>
+        </div>
+    );
+};
+export default InfoView;
