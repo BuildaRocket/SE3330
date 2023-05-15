a70de3a9d1a43bce0b4ad37b9f05b06e6c1b71a6
lightece <317677662@qq.com>
2023-05-15 21:03:03 +0800

    backend jiaoji app

---
+++ src/test/java/com/example/intersection_backend/IntersectionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.intersection_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntersectionBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
